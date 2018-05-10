31bba095db86f8a2641e57215b5307b75d819efb
menashekoren <menashekoren@gmail.com>
2018-05-10 08:11:34 -0400

    Complete stage 11

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,46 @@
 'use strict';
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  // for loop iterating through numberOfRows
+  for (let index = 0; index < numberOfRows; index++) {
+    // Create an empty row array
+    let row = [];
+    // for loop iterating through numberOfColumns
+    for (let index = 0; index < numberOfColumns; index++) {
+      // Push the empty spaces onto the row array  
+      row.push(' ');
+    }
+    // Push the row onto the board array
+    board.push(row);
+  }
+  // Return the board array
+  return board;
+};
+generatePlayerBoard();
 
-const printBoard = board => {
-  console.log('Current Board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-}
-
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-];
-
-printBoard(board);
-board[0][1]='1';
-board[2][2]='B';
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  // for loop iterating through numberOfRows
+  for (let index = 0; index < numberOfRows; index++) {
+    // Create an empty row array
+    let row = [];
+    // for loop iterating through numberOfColumns
+    for (let index = 0; index < numberOfColumns; index++) {
+      // Push the empty spaces onto the row array  
+      row.push(null);
+    }
+    
+    let numberOfBombsPlaced = 0;
+      while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random()) * numberOfRows;
+        let randomColumnIndex = Math.floor(Math.random()) * numberOfColumns;
+        board[randomRowIndex][randomColumnIndex] = 'B'
+      }
+    
+    
+    // Push the row onto the board array
+    board.push(row);
+  }
+  // Return the board array
+  return board;
+};
